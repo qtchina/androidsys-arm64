4a95a89f0bada3c9e35466bf940489a2b09b729e
envsh <yatsen21@outlook.com>
2024-10-16 22:08:20 +0800

    up build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@ cmake . \
   -DCMAKE_ANDROID_NDK=/usr/local/share/android-ndk \
   -DCMAKE_INSTALL_PREFIX=/opt/droidsys-arm64
 
-# libffi
+# libffi/bdwgc/libbacktrace/sqlite2
 CC=/usr/local/share/android-ndk/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang ./configure --target=aarch64-pc-linux-android --host=aarch64-pc-linux-android --build=x86_64-pc-darwin --prefix=/opt/droidsys-arm64/ --enable-shared
 
-# sqlite3
-CC=/usr/local/share/android-ndk/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang ./configure --target=aarch64-pc-linux-android --host=aarch64-pc-linux-android --build=x86_64-pc-darwin --prefix=/opt/droidsys-arm64/ --enable-shared
